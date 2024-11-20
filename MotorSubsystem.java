b7d7aa68e6316f88340668223f0c001ce2b9c5f0
Gorkem <103636907+Gorkem5@users.noreply.github.com>
2024-11-20 22:59:03 +0300

    Update MotorSubsystem.java

--- MotorSubsystem.java
+++ MotorSubsystem.java
@@ -1,17 +1,44 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// Motorları kontrol eden alt sistem sınıfı
 public class MotorSubsystem extends SubsystemBase {
-  /** Creates a new MotorSubsystem. */
-  public MotorSubsystem() {}
+    private final PWMVictorSPX motorA; // A motoru
+// A motoru
+    private final PWMVictorSPX motorB;
+    // Switch mod durumu
+    // B motoru
+    private boolean isSwitchMode = false;
+    // Reverse mod durumu
+    // Switch mod durumu
+    private boolean isReverseMode = false; // Reverse mod durumu
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    // Alt sistemi başlatmak için gerekli port numaralarını alır
+    public MotorSubsystem(int motorPortA, int motorPortB) {
+        motorA = new PWMVictorSPX(motorPortA);
+        motorB = new PWMVictorSPX(motorPortB);
+        motorA.setSafetyEnabled(true); // Motor güvenliği etkin
+        motorB.setSafetyEnabled(true); // Motor güvenliği etkin
+    }
+
+    public boolean isIsSwitchMode() {
+        return isSwitchMode;
+    }
+
+    public void setIsSwitchMode(boolean isSwitchMode) {
+        this.isSwitchMode = isSwitchMode;
+    }
+
+    public boolean isIsReverseMode() {
+        return isReverseMode;
+    }
+
+    public void setIsReverseMode(boolean isReverseMode) {
+        this.isReverseMode = isReverseMode;
+    }
+
+    // Motor hızlarını ayarlar...
+
 }
